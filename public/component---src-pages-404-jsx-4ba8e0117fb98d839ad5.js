b8e6507d9f4235233e94ece7445e04438cbe34db
nenjotsu <renenjo.valente@gmail.com>
2019-06-05 06:07:08 +0800

    Affected: serve

---
+++ public/component---src-pages-404-jsx-4ba8e0117fb98d839ad5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{162:function(e,t,n){"use strict";n.r(t);var a=n(0),o=n.n(a),s=n(1),r=n.n(s),u=n(174),c=function(e){var t=e.location;return o.a.createElement(u.g,{pathname:t.pathname},o.a.createElement("h1",null,"NOT FOUND"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))};t.default=c,c.propTypes={location:r.a.object.isRequired}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-4ba8e0117fb98d839ad5.js.map
